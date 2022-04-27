324a0c178b94bb57b4c1cd4c89fbec8ede07f42b
Shunsuke Fujinawa <>
2022-04-27 16:40:54 +0900

    576px未満の時だけスムーススクロールボタンが表示される

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-$('a[href^="#top"]').click(function () {
+$('#page-top a[href^="#"]').click(function () {
   if ($("html").width() < 576) {
-    var href = $(this).attr("href");
-    // console.log(href);////#topが取得できる
-    var target = $(href == "#top" || href == "" ? 'html' : href); //hrefに入っているのが#topか空白の時は'html'を、どちらでもない時はhrefの値 *ここのhtmlはページトップに戻る
-    console.log(target);
-    var position = target.offset().top;
-    console.log(position);
     var speed = 2000;
-    $("html").animate({
-      scrollTop: position
-    }, speed, "swing");
+    $("html").animate({scrollTop: 0}, speed, "swing");
     return;
     //対象要素.animate(CSSプロパティ、duration時間, easing, 関数) プロパティ部分は{}でオブジェクト形式で記述する必要がある
     //swing→始めと最後は穏やか、途中ちょい早め　　linear→常に同じ速度
